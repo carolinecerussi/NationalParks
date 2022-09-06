693035fb700ab8bc7dc177a7314a9b9c260d8b84
Caroline Cerussi <caroceru@gmail.com>
2022-09-06 15:11:25 -0700

    add swagger code and documentation feature and update finalized ReadMe

--- NationalParks/Startup.cs
+++ NationalParks/Startup.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using NationalParks.Models;
+using Microsoft.OpenApi.Models;
 
 namespace NationalParks
 {
@@ -18,21 +19,40 @@ namespace NationalParks
         public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
-
-        services.AddDbContext<NationalParksContext>(opt =>
+            services.AddDbContext<NationalParksContext>(opt =>
             opt.UseMySql(Configuration["ConnectionStrings:DefaultConnection"], ServerVersion.AutoDetect(Configuration["ConnectionStrings:DefaultConnection"])));
             services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+            
+            services.AddSwaggerGen(options =>
+            {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+        Version = "v1",
+        Title = "NationalParks API",
+        Description = "An ASP.NET Core Web API for managing National Parks info",
+        // TermsOfService = new Uri("https://example.com/terms"),
+        // Contact = new OpenApiContact
+        // {
+        //     Name = "Example Contact",
+        //     Url = new Uri("https://example.com/contact")
+        // },
+        // License = new OpenApiLicense
+        // {
+        //     Name = "Example License",
+        //     Url = new Uri("https://example.com/license")
+        // }
+    });
+});
+}    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseSwagger();
 
-            // app.UseHttpsRedirection();
+            app.UseSwaggerUI();
 
             app.UseRouting();
 
@@ -43,5 +63,5 @@ namespace NationalParks
                 endpoints.MapControllers();
             });
         }
-    }
+}
 }
